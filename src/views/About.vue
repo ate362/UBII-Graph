7805d4aaa23f257cef67c00c8bbcdac5062ac985
ate362 <a_t_e@gmx.de>
2021-05-25 16:05:01 +0200

    graph visualizing

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+  <Error :alert="trigger" :msg="msg"/>
+  <div class="header-editor">
+    <span style="padding-right: 5px;">Sessions:</span>
+    <treeselect v-model="selectedSessionId" @input="showSessionPipeline()" :load-options="loadOps" :options="availableSessions" :auto-load-root-options="false" :multiple="false" placeholder="Sessions..."/>
+  </div>
     <canvas id="canvas" class='litegraph' width='1024' height='720' style='border: 1px solid;'></canvas>
   </div>
 </template>
@@ -9,15 +14,38 @@ import * as litegraph from "litegraph.js";
 // import uuid4 from "uuid";
 import "litegraph.js/css/litegraph.css";
 
+import UbiiClientService from '../ubiiNode/ubiiClientService'
+import { DEFAULT_TOPICS } from '@tum-far/ubii-msg-formats';
+
+import Error from '../components/Error.vue'
+
+// import the component
+import Treeselect from '@riophae/vue-treeselect'
+import { LOAD_ROOT_OPTIONS } from '@riophae/vue-treeselect'
+// import the styles
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+
 export default {
   name: "LiteGraph",
-
+  components: {
+    Treeselect,
+    Error
+  },
   data() {
     return {
       outputContext: null,
       context: null,
       graph: null,
-      lGraphCanvas: null
+      lGraphCanvas: null,
+
+      trigger: 0,
+      msg: '',
+
+      selectedSessionId: null,
+      selectedSession: null,
+      availableSessions: null,
+
+      clientsOfInterest: null
     };
   },
 
@@ -26,43 +54,8 @@ export default {
     this.lGraphCanvas = new litegraph.LGraphCanvas('#canvas', this.graph);
     this.graph.start();
     
-    const client = {
-      id: "d8007d2a-4ea4-4e86-977e-96c7684f800a",
-      name: "ubii-node-webbrowser VueJS Test",
-      devices: [ 
-        {
-          id: "d0c43d91-f94b-4e45-8eaa-9c0b43642c8f",
-          name: "web-example-mouse-pointer",
-          components: [
-            {topic: "/d8007d2a-4ea4-4e86-977e-96c7684f800a/web-example-mouse-pointer/mouse_client_position", ioType: 0, messageFormat: "ubii.dataStructure.Vector2"}, 
-            {topic: "/d8007d2a-4ea4-4e86-977e-96c7684f800a/web-example-mouse-pointer/mirror_mouse", ioType: 0, messageFormat: "bool"}, 
-            {topic: "/d8007d2a-4ea4-4e86-977e-96c7684f800a/web-example-mouse-pointer/mouse_server_position", ioType: 1, messageFormat: "ubii.dataStructure.Vector2"}
-          ]
-        }
-      ]
-    }
-
-    const session = {
-      id: "ed51046d-7552-4fee-8d25-ced773e785ae",
-      name: "web-mouse-example-session",
-      processingModules: [{
-        id: "8ce44fda-a037-4457-84e0-dc824fc9689a",
-        name: "mirror-mouse-pointer",
-        inputs: [{internalName: "clientPointer", messageFormat: "ubii.dataStructure.Vector2"},{internalName: "mirrorPointer", messageFormat: "bool"}],
-        outputs: [{internalName: "serverPointer", messageFormat: "ubii.dataStructure.Vector2"}]
-      }]
-    }
-
     litegraph.LiteGraph.clearRegisteredTypes()
-
-    client.devices.forEach(device => {
-      this.registerClientNode(client.name, device, device.components)
-    })
-    
-    session.processingModules.forEach(proc => {
-      this.registerProcessNode(session.name, proc, proc.inputs, proc.outputs)
-    })
-
+        
   },
   methods: {
     registerProcessNode: function(sname, proc, inp, out) {
@@ -77,7 +70,6 @@ export default {
           this.addOutput(o.internalName, o.messageFormat)
         })
 
-        this.properties = { precision: true };
       }
       node.title = proc.name
 
@@ -127,17 +119,146 @@ export default {
       //register in the system
       litegraph.LiteGraph.registerNodeType("Clients/"+clientName+"/"+dev.name, node)
     },
-    addNode: function(id, device, components) {
+    addNode: function(name, pos) {
 
-      console.log(id, device, components)
-
-      var node_const = litegraph.createNode("basic/const");
-      node_const.pos = [200,200];
+      var node_const = litegraph.LiteGraph.createNode(name);
+      node_const.pos = pos;
       this.graph.add(node_const);
-      node_const.setValue(4.5);
+    },
+    loadProcModules: async function () {
+       const res = (UbiiClientService.isConnected()) ? await UbiiClientService.client.callService(
+          {
+            topic: DEFAULT_TOPICS.SERVICES.SESSION_RUNTIME_GET_LIST
+          }
+        ) : null
+
+        const sList = res?.sessionList ?? {}
+        // console.log(sList)
+        try { 
+          this.selectedSession = sList.filter(val => val.status === 1 && this.selectedSessionId == val.id)[0]
+        } catch {
+          this.msg = 'Selected Session was not found.'
+          this.trigger= !this.trigger
+        }
+    },
+    registerSessionNodes: async function () {
+      this.selectedSession.processingModules.forEach(proc => {
+        this.registerProcessNode(this.selectedSession.name, proc, proc.inputs, proc.outputs)
+      })
+    },
+    loadComponents: async function () {
+       
+      const clientsDevices = await this.selectedSession.ioMappings.map(val => {
+        const inputArray = val.inputMappings.map(val => {
+          const sp = val.topic.split('/')
+          return sp[1]+'.'+sp[2]
+        })
+        const outputArray = val.outputMappings.map(val => {
+          const sp = val.topic.split('/')
+          return sp[1]+'.'+sp[2]
+        })
+        return [...new Set(inputArray.concat(outputArray))][0]
+      })
+      await this.loadClients(clientsDevices)
+    },
+    loadClients: async function (filts) {
+
+      const res = (UbiiClientService.isConnected()) ? await UbiiClientService.client.callService(
+        {
+          topic: DEFAULT_TOPICS.SERVICES.CLIENT_GET_LIST
+        }
+      ) : null
+      
+      const sList = res?.elements ?? {}
+      
+      const clientFilts = filts.map(val => {
+        return val.split('.')[0]
+      })
+
+      const deviceFilts = filts.map(val => {
+        return val.split('.')[1]
+      })
 
 
+      try {
+        this.clientsOfInterest = sList.filter(val => val.state === 2 && clientFilts.includes(val.id))
+        this.clientsOfInterest.forEach(client => {
+          client.devices = client.devices.filter(val => deviceFilts.includes(val.name))
+        })
+      } catch {
+        this.msg = 'No Clients for this session are available.'
+        this.trigger= !this.trigger
+      }
+      
+    },
+    registerClientNodes: async function () {
+      this.clientsOfInterest.forEach(client => {
+        client.devices.forEach(device => {
+          this.registerClientNode(client.name, device, device.components)
+        })
+      })
+    },
+    calcPostions: async function () {
+      console.log('TODO CALC POSITIONS')
+    },
+    addSessionNodes: async function () {
+      this.selectedSession.processingModules.forEach(proc => {
+        proc.position = [600,200];
+        this.addNode("Sessions/"+this.selectedSession.name+"/"+proc.name, proc.position )
+      })
+    },
+    addClientNodes: async function () {
+      this.clientsOfInterest.forEach(client => {
+        client.devices.forEach(device => {
+          device.position = [200,200];
+          this.addNode("Clients/"+client.name+"/"+device.name, device.position)
+        })
+      })
+    },
+    showSessionPipeline: async function() {
+      await this.loadProcModules()
+      await this.registerSessionNodes()
+      await this.loadComponents()
+      await this.registerClientNodes()
+      await this.calcPostions()
+      await this.addClientNodes()
+      await this.addSessionNodes()
+      // console.log(this.selectedSession)
+    },
+    loadOps: async function({ action/*, callback*/ }) {
+      if (action === LOAD_ROOT_OPTIONS) {
+
+        const res = (UbiiClientService.isConnected()) ? await UbiiClientService.client.callService(
+          {
+            topic: DEFAULT_TOPICS.SERVICES.SESSION_RUNTIME_GET_LIST
+          }
+        ) : null
+    
+        const sList = res?.sessionList ?? {}
+        // console.log(sList)
+        try { 
+          this.availableSessions = sList.filter(val => val.status === 1).map(val => {
+              return { 
+                id: val.id,
+                label: `${val.name}`
+              }
+            })
+        } catch {
+          this.msg = 'No sessions available.'
+          this.trigger= !this.trigger// console.log('An empty or a invalid response from the server.')
+        }
+      }
     }
   }
 }
 </script>
+
+<style scoped>
+.header-editor {
+  border: 1px solid;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
